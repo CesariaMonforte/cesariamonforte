b224ce7b36620a404a1b330e9615b2b50c1cfa8c
Cesaria Monforte <cmonforte@se14l0121-03.ad.bcit.ca>
2026-02-23 12:09:31 -0800

    Adjusting transform in styles and adjusting book.js to include side class

--- src/_ui/Book/Book.jsx
+++ src/_ui/Book/Book.jsx
@@ -3,12 +3,12 @@ import styles from "./Book.module.css";
 function Book() {
   return (
     <div className={styles.book}>
-      <div className={styles.spine}>
+      <div className={`${styles.side} ${styles.spine}`}>
         <span className={styles.title}>Book title</span>
         <span className={styles.author}>author</span>
       </div>
-      <div className={styles.top}></div>
-      <div className={styles.cover}></div>
+      <div className={`${styles.side} ${styles.top}`}></div>
+      <div className={`${styles.side} ${styles.cover}`}></div>
     </div>
   );
 }
