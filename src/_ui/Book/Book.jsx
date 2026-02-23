a90bc7cb27da9671e3bfa3620c2079e961185f28
Cesaria Monforte <cmonforte@se14l0121-03.ad.bcit.ca>
2026-02-23 11:25:35 -0800

    Adding Book title and author to spine

--- src/_ui/Book/Book.jsx
+++ src/_ui/Book/Book.jsx
@@ -3,7 +3,10 @@ import styles from "./Book.module.css";
 function Book() {
   return (
     <div className={styles.book}>
-      <div className={styles.spine}></div>
+      <div className={styles.spine}>
+        <span className={styles.title}>Book title</span>
+        <span className={styles.author}>author</span>
+      </div>
       <div className={styles.top}></div>
       <div className={styles.cover}></div>
     </div>
